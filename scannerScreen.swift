76429e22493a858c2efa651a79c8276b7037e01b
Akshay Yadav <akshay-yadav@uiowa.edu>
2016-04-19 04:05:49 -0500

    working QRCode Scanner and stop running implemented

---
+++ scannerScreen.swift
@@ -0,0 +1,12 @@
+//
+//  scannerScreen.swift
+//  CMPiPad
+//
+//  Created by Akshay's on 4/18/16.
+//  Copyright © 2016 Akshay's. All rights reserved.
+//
+
+
+class scannerScreen: UIViewController {
+
+}
